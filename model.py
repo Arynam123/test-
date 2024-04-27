563143494132b93ade4999aed1c2c390fcc513bc
Arynam123 <141307890+Arynam123@users.noreply.github.com>
2024-04-27 21:21:59 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import pickle
-from collections.abc import Mapping
+from collections import Mapping
 
 
 # Load the dataset
